86137f660dfa1188a6c1ba27c7a35670b1cc26c0
Alex Vasile <1995.alex@gmail.com>
2025-07-09 16:55:32 +0300

    Product entity

---
+++ src/main/java/com/example/storeApi/model/Product.java
@@ -0,0 +1,30 @@
+package com.example.storeApi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Double price;
+
+    @NotNull
+    private String category;
+}
